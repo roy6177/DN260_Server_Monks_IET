af4db66a31bb301a3df5d6266cf3193d4057b1aa
AC <aneekneel1234@gmai.com>
2020-08-01 04:00:28 +0530

    Android App - First Commit

---
+++ Android src/main/java/com/example/inditribesv1/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.inditribesv1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView)findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("https://inditribes.co.in/");
+        webView.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# DN260_Soham_Server_Monks
+(Inditribes) E-Marketplace for tribals.
+
+src folder contains the source files for the app
